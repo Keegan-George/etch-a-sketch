ae46b62d31a15b3d493fdc9a88d8fe1a2730822b
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-24 10:35:09 -0400

    Add docstrings for each function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,15 @@
 let canDraw = false;
 
-function createGrid(size = 16) {
+/**
+ * Creates a grid of size n x n, where n is the argument passed to the function. 
+ */
+function createGrid(n = 16) {
     const gridContainer = document.querySelector(".grid-container");
 
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < n; i++) {
         column = document.createElement("div");
         column.classList.add("column");
-        for (let j = 0; j < size; j++) {
+        for (let j = 0; j < n; j++) {
             square = document.createElement("div");
             square.classList.add("square");
             column.appendChild(square);
@@ -15,18 +18,29 @@ function createGrid(size = 16) {
     }
 }
 
+/**
+ * Deletes the grid from the screen
+ */
+
 function deleteGrid() {
     columns = document.querySelectorAll(".column");
     columns.forEach(column => { column.remove() });
 }
 
+/**
+ * Prompts the user to enter the value of a new grid. 
+ * Deletes the previous grid and creates a new one
+ */
 function resetGrid() {
     let size = +prompt("Enter the size of the new grid.");
     deleteGrid();
     createGrid(size);
 }
 
-function setupListeners(){
+/**
+ * Sets up the listeners for the grids. 
+ */
+function setupListeners() {
     const gridContainer = document.querySelector(".grid-container");
 
     gridContainer.addEventListener("mousedown", event => {
@@ -38,10 +52,11 @@ function setupListeners(){
     })
 
     gridContainer.addEventListener("mouseover", event => {
-        if (canDraw){
+        if (canDraw) {
             event.target.classList.add("enabled");
+            console.log(event.button)
         }
-        
+
     });
 }
 
