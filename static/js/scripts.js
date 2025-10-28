4a99f08f5a133d116ce513c2520af13055b1a592
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-10-28 09:42:26 -0400

    Implement new progressive darkening logic
    The previous approach of reducing the RGB colours by 10% would
    never reach 0 in 10 clicks (i.e. 0.9^10 ~= 0.35). Instead,
    this new approach takes the largest of the RGB values  and divides
    by 10. This value is then stored and used to reduce the color by
    that amount, each time that particular square is clicked.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -49,7 +49,6 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-
 /**
  * Returns a randomly generated RGB color string.
  */
@@ -66,17 +65,23 @@ function hasBackgroundColor(event) {
 }
 
 /**
+ * 
+ */
+function getDarkeningRate(color) {
+    current_color = color.match(/\d+/g);
+    values = current_color.map(n => +n);
+    max_value = Math.max(...values);
+    return Math.ceil(max_value / 10);
+}
+
+/**
  * Return
  */
-function darken(color) {
+function darken(color, amount) {
     current_color = color.match(/\d+/g);
-    let [r, g, b] = current_color;
-    r = r * 0.9;
-    g = g * 0.9;
-    b = b * 0.9;
-
-    darkened_color = `rgb(${r},${g},${b})`;
-    return darkened_color;
+    darkened_color = current_color.map(n => +n - amount);
+    let [r, g, b] = darkened_color;
+    return `rgb(${r},${g},${b})`;
 }
 
 /**
@@ -88,18 +93,23 @@ function setupListeners() {
     gridContainer.addEventListener("mousedown", event => {
         canDraw = true;
 
-        if (hasBackgroundColor(event)) {
+        const backgroundColor = event.target.style.backgroundColor;
+
+        if (backgroundColor) {
             current_color = event.target.style.backgroundColor;
             console.log(`current color: ${current_color}`);
-
-            darkened_color = darken(current_color);
+            darken_rate = event.target.dataset.progressingDarkenStepSize;
+            darkened_color = darken(current_color, darken_rate);
+            console.log(`darken color: ${darkened_color}`);
 
             event.target.style.backgroundColor = darkened_color;
             console.log(`darken color: ${event.target.style.backgroundColor}`);
         }
 
         else {
-            event.target.style.backgroundColor = getRandomRGBColor();
+            color = getRandomRGBColor();
+            event.target.style.backgroundColor = color;
+            event.target.dataset.progressingDarkenStepSize = getDarkeningRate(color);
         }
     });
 
@@ -109,18 +119,23 @@ function setupListeners() {
 
     gridContainer.addEventListener("mouseover", event => {
         if (canDraw) {
-            if (hasBackgroundColor(event)) {
+            const backgroundColor = event.target.style.backgroundColor;
+
+            if (backgroundColor) {
                 current_color = event.target.style.backgroundColor;
                 console.log(`current color: ${current_color}`);
-
-                darkened_color = darken(current_color);
+                darken_rate = event.target.dataset.progressingDarkenStepSize;
+                darkened_color = darken(current_color, darken_rate);
+                console.log(`darken color: ${darkened_color}`);
 
                 event.target.style.backgroundColor = darkened_color;
                 console.log(`darken color: ${event.target.style.backgroundColor}`);
             }
 
             else {
-                event.target.style.backgroundColor = getRandomRGBColor();
+                color = getRandomRGBColor();
+                event.target.style.backgroundColor = color;
+                event.target.dataset.progressingDarkenStepSize = getDarkeningRate(color);
             }
         }
     });
